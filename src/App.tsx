6b9d5e3eee8d610a9626fa1bc300386ef2861ecd
sharif-jobayed <sharif.jobayed@gmail.com>
2026-03-20 00:11:47 +0600

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './pages/Layout';
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
+import { ScrollToTop } from './components/ScrollToTop';
  
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
