a9150c032ab453b902a4c0b98404693584386b70
sharif-jobayed <sharif.jobayed@gmail.com>
2026-03-20 00:19:54 +0600

    ...

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -2,11 +2,21 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (hash) {
+      // Use setTimeout to allow the page layout to render before scrolling
+      setTimeout(() => {
+        const element = document.getElementById(hash.slice(1));
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 0);
+    } else {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 
   return null;
 }
